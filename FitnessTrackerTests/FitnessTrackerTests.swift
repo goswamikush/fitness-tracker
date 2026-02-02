16b066f0af6bda15726cfcacb92d65e3816d3f64
Kush Goswami <kushvgoswami84@gmail.com>
2026-02-01 20:34:58 -0500

    Initial Commit

---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by Kush Goswami on 2/1/26.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
