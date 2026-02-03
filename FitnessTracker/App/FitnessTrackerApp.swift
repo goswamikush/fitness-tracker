c5771cf657e5ef9551f956a9cf2ed65983b05669
Kush Goswami <kushvgoswami84@gmail.com>
2026-02-02 20:21:44 -0500

    Cleanup project

--- FitnessTracker/App/FitnessTrackerApp.swift
+++ FitnessTracker/App/FitnessTrackerApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct FitnessTrackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
