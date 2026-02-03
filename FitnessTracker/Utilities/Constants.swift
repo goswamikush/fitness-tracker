bb80cc8b3a3d45838a48c6db98306fc348868d1b
Kush Goswami <kushvgoswami84@gmail.com>
2026-02-02 20:32:57 -0500

    Add Constants.swift

---
+++ FitnessTracker/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FitnessTracker
+//
+//  Created by Kush Goswami on 2/2/26.
+//
+
+import SwiftUI
+
+struct AppColors {
+    static let background = Color(red: 18/255, green: 18/255, blue: 18/255)
+}
