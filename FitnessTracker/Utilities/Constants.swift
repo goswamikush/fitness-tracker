b1052d294ba09e4ae32d64c7e0f051958ad59aab
Kush Goswami <kushvgoswami84@gmail.com>
2026-02-14 13:53:52 -0500

    Add macro colors

--- FitnessTracker/Utilities/Constants.swift
+++ FitnessTracker/Utilities/Constants.swift
@@ -12,3 +12,10 @@ struct AppColors {
     static let cardBackground = Color(red: 26/255, green: 26/255, blue: 26/255)
 }
 
+struct MacroColors {
+    static let carbs = Color(red: 66/255, green: 240/255, blue: 153/255)
+    static let protein = Color(red: 81/255, green: 166/255, blue: 251/255)
+    static let fats = Color(red: 250/255, green: 185/255, blue: 56/255)
+
+}
+
